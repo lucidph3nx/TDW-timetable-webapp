e9f433f8d307304f8d2b1481c86cdad56b4a3099
lucidph3nx <ph3nx11@gmail.com>
2018-07-20 10:18:21 +1200

    fixes

--- currentTimetable.js
+++ currentTimetable.js
@@ -1,14 +1,14 @@
 function getCurrentTimetable() {
-
-    var TimetableChangeDate = new Date("11/19/2017");
+    //I dont know why but this needs to go in the format MM/DD/YYYY stupid americans
+    var TimetableChangeDate = new Date("07/15/2018");
     var todaysDate = new Date();
     TimetableChangeDate.setHours(0,0,0,0);
     todaysDate.setHours(0,0,0,0);
     // Check if today is past the timetable change date
     if(todaysDate < TimetableChangeDate){
-        return "timetabledata_10012018.js";
-    } else {
         return "timetabledata_10032018.js";
+    } else {
+        return "timetabledata_15072018.js";
     }
 
 };
