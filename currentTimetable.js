2330a8d0d32b8bd6bc1744888e0babbef4781238
lucidph3nx <ph3nx11@gmail.com>
2020-04-02 11:27:50 +1300

    fixes for updatability

--- currentTimetable.js
+++ currentTimetable.js
@@ -1,14 +1,19 @@
-function getCurrentTimetable() {
-    //I dont know why but this needs to go in the format MM/DD/YYYY stupid americans
-    var TimetableChangeDate = new Date("02/24/2019");
-    var todaysDate = new Date();
-    TimetableChangeDate.setHours(0,0,0,0);
-    todaysDate.setHours(0,0,0,0);
-    // Check if today is past the timetable change date
-    if(todaysDate < TimetableChangeDate){
-        return "timetabledata_15072018.js";
-    } else {
-        return "timetabledata_24022019.js";
-    }
 
+/**
+ * decides which timetable file to use on load
+ * @return {string} timetable file name
+ */
+function getCurrentTimetable() {
+  const oldTimetable = 'timetabledata_15072018.js';
+  const newTimetable = 'timetabledata_24022019.js';
+  const timetableChangeDate = new Date('02/24/2019'); // needs to go in the format MM/DD/YYYY stupid americans
+  const todaysDate = new Date();
+  timetableChangeDate.setHours(0, 0, 0, 0);
+  todaysDate.setHours(0, 0, 0, 0);
+  // Check if today is past the timetable change date
+  if (todaysDate < timetableChangeDate) {
+    return oldTimetable;
+  } else {
+    return newTimetable;
+  }
 };
